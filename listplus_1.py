2b449ce6c308d13455f5d89b65614b3e1cbdb6a7
nattybear <vpark45@gmail.com>
2018-12-09 15:45:12 +0900

    Fix coding style (#1)
    * Fix coding style
    
    Delete one space between function name and parenthesis. It's about coding style also known as the Coding Convention. See PEP8.
    
    * Modify the variable name from underscore to i
    
    When you ignore the elements of list, use the underscore. Otherwise use 'i' for indexing.

--- listplus_1.py
+++ listplus_1.py
@@ -1,10 +1,10 @@
-def listplus_1 (A): 
-    for _ in range(len(A)):
-        A[_] = A[_] + 1
-        
+def listplus_1(A): 
+    for i in range(len(A)):
+        A[i] = A[i] + 1
+
     return A 
 
 makeA = input().split()
-makeA = list(map(int,makeA))
+makeA = list(map(int, makeA))
 
 print(listplus_1(makeA))
